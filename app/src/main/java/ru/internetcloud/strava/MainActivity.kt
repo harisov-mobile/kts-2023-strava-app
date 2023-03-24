7ef57739a9fee65865a5731652ef326ae7e8f7b7
harisov-mobile <v8spec@mail.ru>
2023-03-24 12:36:08 +0100

    Add second fragment

--- app/src/main/java/ru/internetcloud/strava/MainActivity.kt
+++ app/src/main/java/ru/internetcloud/strava/MainActivity.kt
@@ -1,36 +1,84 @@
 package ru.internetcloud.strava
 
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.color.MaterialColors
 import ru.internetcloud.strava.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnBoardingFragment.OnBoardingEvents {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private var onBoardingFragment: OnBoardingFragment? = null
+
+    companion object {
+        const val FRAGMENT_ONBOARDING_TAG = "on_boarding_fragment"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(binding.root)
 
         if (savedInstanceState == null) {
-            showFragment(fragment = OnBoardingFragment(), withBackStack = false)
+            hideBars()
+            showFragment(fragment = OnBoardingFragment(), withBackStack = false, tag = FRAGMENT_ONBOARDING_TAG)
+        } else {
+            onBoardingFragment =
+                supportFragmentManager.findFragmentByTag(FRAGMENT_ONBOARDING_TAG) as? OnBoardingFragment
+            if (onBoardingFragment == null) {
+                showBars()
+            } else {
+                hideBars()
+            }
         }
     }
 
-    private fun showFragment(fragment: Fragment, withBackStack: Boolean) {
+    private fun showFragment(fragment: Fragment, withBackStack: Boolean, tag: String? = null) {
         val transaction = supportFragmentManager.beginTransaction()
 
         if (withBackStack) {
-            transaction.replace(R.id.fragment_container, fragment)
-                .addToBackStack(null)
-                .commit()
-        } else {
-            transaction.add(R.id.fragment_container, fragment)
-                .commit()
+            transaction.addToBackStack(null)
         }
+
+        transaction.replace(R.id.fragment_container, fragment, tag)
+            .commit()
+    }
+
+    override fun onShowSecondFragment() {
+        showBars()
+        val fragment = SecondFragment()
+        showFragment(fragment = fragment, withBackStack = false)
+    }
+
+    private fun hideBars() {
+        getSupportActionBar()?.hide() // hide the title bar
+        val window: Window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS) // hide status bar
+    }
+
+    private fun showBars() {
+        val colorPrimaryVariant =
+            MaterialColors.getColor(
+                this,
+                com.google.android.material.R.attr.colorPrimaryVariant,
+                "no colorPrimaryVariant definition"
+            )
+
+        val window = window
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            )
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = colorPrimaryVariant
+
+        getSupportActionBar()?.show() // show the title bar
     }
 }
--- app/src/main/java/ru/internetcloud/strava/OnBoardingFragment.kt
+++ app/src/main/java/ru/internetcloud/strava/OnBoardingFragment.kt
@@ -1,5 +1,6 @@
 package ru.internetcloud.strava
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,13 +10,37 @@ import ru.internetcloud.strava.databinding.FragmentOnBoardingBinding
 
 class OnBoardingFragment : Fragment() {
 
+    interface OnBoardingEvents {
+        fun onShowSecondFragment()
+    }
+
+    var hostActivity: OnBoardingEvents? = null
+
     private var _binding: FragmentOnBoardingBinding? = null
     private val binding: FragmentOnBoardingBinding
         get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        hostActivity = context as OnBoardingEvents
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        hostActivity = null
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentOnBoardingBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.getStartedButton.setOnClickListener {
+            hostActivity?.onShowSecondFragment()
+        }
     }
 
     override fun onDestroyView() {
