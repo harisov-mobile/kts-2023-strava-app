c235551dcf625824edf39a32eae12af9a875e4a3
harisov-mobile <v8spec@mail.ru>
2023-05-17 09:08:58 +0200

    Lift assigment out of if

--- app/src/main/java/ru/internetcloud/strava/data/repository/ProfileRepositoryImpl.kt
+++ app/src/main/java/ru/internetcloud/strava/data/repository/ProfileRepositoryImpl.kt
@@ -15,16 +15,16 @@ class ProfileRepositoryImpl : ProfileRepository {
 
         try {
             val networkResponse = StravaApiFactory.profileApi.getProfile()
-            if (networkResponse.isSuccessful) {
+            result = if (networkResponse.isSuccessful) {
                 val stravaAthleteDTO = networkResponse.body()
-                result = stravaAthleteDTO?.let { currentDTO ->
+                stravaAthleteDTO?.let { currentDTO ->
                     val stravaAthlete = stravaAthleteMapper.fromDtoToDomain(currentDTO)
                     DataResponse.Success(stravaAthlete)
                 } ?: let {
                     DataResponse.Error(exception = java.lang.IllegalStateException("No athlete found"))
                 }
             } else {
-                result = DataResponse.Error(Exception(networkResponse.errorBody()?.string().orEmpty()))
+                DataResponse.Error(Exception(networkResponse.errorBody()?.string().orEmpty()))
             }
         } catch (e: Exception) {
             result = DataResponse.Error(e)
