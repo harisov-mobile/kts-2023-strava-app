a1abf42d6a09bf8dbdcc5c60287f8208651db567
harisov-mobile <v8spec@mail.ru>
2023-05-19 12:40:05 +0200

    Delete unnecessary classes

--- app/src/main/java/ru/internetcloud/strava/presentation/oldmain/model/SimpleItem.kt
+++
@@ -1,8 +0,0 @@
-package ru.internetcloud.strava.presentation.oldmain.model
-
-data class SimpleItem(
-    override val id: Int,
-    val title: String,
-    val author: String,
-    val likesCount: Int
-) : Item
