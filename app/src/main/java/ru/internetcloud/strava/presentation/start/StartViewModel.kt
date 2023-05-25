777d3e5c664b8a928004b4453ba304d4defec686
harisov-mobile <v8spec@mail.ru>
2023-05-25 06:37:13 +0200

    Navigate to MainFragment if token exists in SharedPrefs

--- app/src/main/java/ru/internetcloud/strava/presentation/start/StartViewModel.kt
+++ app/src/main/java/ru/internetcloud/strava/presentation/start/StartViewModel.kt
@@ -3,14 +3,21 @@ package ru.internetcloud.strava.presentation.start
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavDirections
 import ru.internetcloud.strava.data.onboarding.repository.OnboardingRepositoryImpl
+import ru.internetcloud.strava.data.token.repository.TokenRepositoryImpl
 import ru.internetcloud.strava.domain.onboarding.usecase.GetOnboardingParamsUseCase
 import ru.internetcloud.strava.domain.onboarding.usecase.SaveOnboardingParamsUseCase
+import ru.internetcloud.strava.domain.token.usecase.GetTokenUseCase
+import ru.internetcloud.strava.domain.token.usecase.RememberTokenUseCase
 
 class StartViewModel : ViewModel() {
 
     private val onboardingRepository = OnboardingRepositoryImpl()
     private val getOnboardingParamsUseCase = GetOnboardingParamsUseCase(onboardingRepository)
     private val saveOnboardingParamsUseCase = SaveOnboardingParamsUseCase(onboardingRepository)
+
+    private val tokenRepository = TokenRepositoryImpl()
+    private val getTokenUseCase = GetTokenUseCase(tokenRepository)
+    private val rememberTokenUseCase = RememberTokenUseCase(tokenRepository)
 
     fun getDirectionToNavigate(): NavDirections {
         val onboardingParams = getOnboardingParamsUseCase.getOnboardingParams()
@@ -19,7 +26,14 @@ class StartViewModel : ViewModel() {
             saveOnboardingParamsUseCase.saveOnboardingParams()
             StartFragmentDirections.actionStartFragmentToOnBoardingFragment()
         } else {
-            StartFragmentDirections.actionStartFragmentToAuthFragment()
+            val token = getTokenUseCase.getToken()
+
+            if (token.accessToken.isNullOrEmpty()) {
+                StartFragmentDirections.actionStartFragmentToAuthFragment()
+            } else {
+                rememberTokenUseCase.rememberToken(token)
+                StartFragmentDirections.actionStartFragmentToMainFragment()
+            }
         }
     }
 }
