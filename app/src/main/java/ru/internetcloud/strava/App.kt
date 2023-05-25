4c1c98b450478c0b7b532623b0d96682664a2e04
harisov-mobile <v8spec@mail.ru>
2023-05-25 05:24:26 +0200

    Add TokenSharedPreferencesStorage

--- app/src/main/java/ru/internetcloud/strava/App.kt
+++ app/src/main/java/ru/internetcloud/strava/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import ru.internetcloud.strava.data.common.ErrorMessageConverter
 import ru.internetcloud.strava.data.common.StravaApiFactory
 import ru.internetcloud.strava.data.onboarding.OnboardingSharedPreferencesStorage
+import ru.internetcloud.strava.data.token.TokenSharedPreferencesStorage
 import timber.log.Timber
 
 class App : Application() {
@@ -13,6 +14,7 @@ class App : Application() {
         StravaApiFactory.init(this)
         ErrorMessageConverter.init(this)
         OnboardingSharedPreferencesStorage.init(this)
+        TokenSharedPreferencesStorage.init(this)
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
---
+++ app/src/main/java/ru/internetcloud/strava/data/token/TokenSharedPreferencesStorage.kt
@@ -0,0 +1,34 @@
+package ru.internetcloud.strava.data.token
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import ru.internetcloud.strava.data.auth.network.model.TokensModel
+
+object TokenSharedPreferencesStorage {
+
+    private const val TOKEN_SHARED_PREFS_NAME = "token_shared_prefs"
+    private const val KEY_ACCESS_TOKEN = "key_access_token"
+    private const val KEY_REFRESH_TOKEN = "key_refresh_token"
+    private const val KEY_ID_TOKEN = "key_id_token"
+
+    private lateinit var tokenSharedPrefs: SharedPreferences
+
+    fun init(applicaton: Application) {
+        tokenSharedPrefs = applicaton.getSharedPreferences(TOKEN_SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+    }
+
+    fun getTokenData(): TokensModel {
+        return TokensModel(
+            accessToken = tokenSharedPrefs.getString(KEY_ACCESS_TOKEN, null),
+            refreshToken = tokenSharedPrefs.getString(KEY_REFRESH_TOKEN, null),
+            idToken = tokenSharedPrefs.getString(KEY_ID_TOKEN, null)
+        )
+    }
+
+    fun saveTokenData(tokensModel: TokensModel) {
+        tokenSharedPrefs.edit().putString(KEY_ACCESS_TOKEN, tokensModel.accessToken).apply()
+        tokenSharedPrefs.edit().putString(KEY_REFRESH_TOKEN, tokensModel.refreshToken).apply()
+        tokenSharedPrefs.edit().putString(KEY_ID_TOKEN, tokensModel.idToken).apply()
+    }
+}
