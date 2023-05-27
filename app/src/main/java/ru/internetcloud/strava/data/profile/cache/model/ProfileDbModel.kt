e09f57d90eb2666fa7465586a967e75d42bb96b7
harisov-mobile <v8spec@mail.ru>
2023-05-27 14:44:32 +0200

    Fix ProfileDbModel - add room annotation

--- app/src/main/java/ru/internetcloud/strava/data/profile/cache/model/ProfileDbModel.kt
+++ app/src/main/java/ru/internetcloud/strava/data/profile/cache/model/ProfileDbModel.kt
@@ -1,6 +1,11 @@
 package ru.internetcloud.strava.data.profile.cache.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "profile")
 data class ProfileDbModel(
+    @PrimaryKey
     val id: Long,
     val firstName: String,
     val lastName: String,
