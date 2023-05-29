4a06f27ff0ec9e0dd57998b23e4aaa26cc36d15a
harisov-mobile <v8spec@mail.ru>
2023-05-29 09:27:12 +0200

    Add TrainingContract

---
+++ app/src/main/java/ru/internetcloud/strava/data/training/cache/model/TrainingContract.kt
@@ -0,0 +1,15 @@
+package ru.internetcloud.strava.data.training.cache.model
+
+object TrainingContract {
+    const val TABLE_NAME = "trainings"
+
+    object Columns {
+        const val ID = "id"
+        const val NAME = "name"
+        const val DISTANCE = "distance"
+        const val MOVING_TIME = "moving_time"
+        const val TYPE = "type"
+        const val START_DATE = "start_date"
+        const val DESCRIPTION = "description"
+    }
+}
--- app/src/main/java/ru/internetcloud/strava/data/training/cache/model/TrainingDbModel.kt
+++ app/src/main/java/ru/internetcloud/strava/data/training/cache/model/TrainingDbModel.kt
@@ -1,17 +1,31 @@
 package ru.internetcloud.strava.data.training.cache.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.Date
 
-@Entity(tableName = "trainings")
+@Entity(tableName = TrainingContract.TABLE_NAME)
 data class TrainingDbModel(
     @PrimaryKey
+    @ColumnInfo(name = TrainingContract.Columns.ID)
     val id: Long,
+
+    @ColumnInfo(name = TrainingContract.Columns.NAME)
     val name: String,
+
+    @ColumnInfo(name = TrainingContract.Columns.DISTANCE)
     val distance: Float,
+
+    @ColumnInfo(name = TrainingContract.Columns.MOVING_TIME)
     val movingTime: Int,
+
+    @ColumnInfo(name = TrainingContract.Columns.TYPE)
     val type: String,
+
+    @ColumnInfo(name = TrainingContract.Columns.START_DATE)
     val startDate: Date,
+
+    @ColumnInfo(name = TrainingContract.Columns.DESCRIPTION)
     val description: String
 )
