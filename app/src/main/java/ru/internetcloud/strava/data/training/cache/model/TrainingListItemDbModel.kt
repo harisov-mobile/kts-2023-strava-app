14f403313fc6807eaebe2e8899b13c9410699792
harisov-mobile <v8spec@mail.ru>
2023-05-29 09:33:07 +0200

    Add TrainingListItemContract

--- app/src/main/java/ru/internetcloud/strava/data/training/cache/model/TrainingListItemDbModel.kt
+++ app/src/main/java/ru/internetcloud/strava/data/training/cache/model/TrainingListItemDbModel.kt
@@ -1,16 +1,28 @@
 package ru.internetcloud.strava.data.training.cache.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.Date
 
-@Entity(tableName = "training_list_items")
+@Entity(tableName = TrainingListItemContract.TABLE_NAME)
 data class TrainingListItemDbModel(
     @PrimaryKey
+    @ColumnInfo(name = TrainingContract.Columns.ID)
     val id: Long,
+
+    @ColumnInfo(name = TrainingContract.Columns.NAME)
     val name: String,
+
+    @ColumnInfo(name = TrainingContract.Columns.DISTANCE)
     val distance: Float,
+
+    @ColumnInfo(name = TrainingContract.Columns.MOVING_TIME)
     val movingTime: Int,
+
+    @ColumnInfo(name = TrainingContract.Columns.TYPE)
     val type: String,
+
+    @ColumnInfo(name = TrainingContract.Columns.START_DATE)
     val startDate: Date
 )
