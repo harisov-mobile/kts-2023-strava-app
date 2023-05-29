5068952913bf570374279546291b2f29bbd41a85
harisov-mobile <v8spec@mail.ru>
2023-05-29 19:28:24 +0200

    Before Android Studio update

---
+++ app/src/main/java/ru/internetcloud/strava/presentation/training/detail/TrainingAddScreen.kt
@@ -0,0 +1,10 @@
+package ru.internetcloud.strava.presentation.training.detail
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ShowTrainingAddScreen(
+
+) {
+
+}
---
+++ app/src/main/java/ru/internetcloud/strava/presentation/training/detail/TrainingAddViewModel.kt
@@ -0,0 +1,6 @@
+package ru.internetcloud.strava.presentation.training.detail
+
+import androidx.lifecycle.ViewModel
+
+class TrainingAddViewModel : ViewModel() {
+}
