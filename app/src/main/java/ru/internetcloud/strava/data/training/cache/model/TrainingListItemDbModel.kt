b1b78c4fb270c8bfb31fb49983bf4f97cded64cf
harisov-mobile <v8spec@mail.ru>
2023-06-06 08:30:41 +0300

    Fix TrainingListItemDbModel

--- app/src/main/java/ru/internetcloud/strava/data/training/cache/model/TrainingListItemDbModel.kt
+++ app/src/main/java/ru/internetcloud/strava/data/training/cache/model/TrainingListItemDbModel.kt
@@ -8,21 +8,21 @@ import java.util.Date
 @Entity(tableName = TrainingListItemContract.TABLE_NAME)
 data class TrainingListItemDbModel(
     @PrimaryKey
-    @ColumnInfo(name = TrainingContract.Columns.ID)
+    @ColumnInfo(name = TrainingListItemContract.Columns.ID)
     val id: Long,
 
-    @ColumnInfo(name = TrainingContract.Columns.NAME)
+    @ColumnInfo(name = TrainingListItemContract.Columns.NAME)
     val name: String,
 
-    @ColumnInfo(name = TrainingContract.Columns.DISTANCE)
+    @ColumnInfo(name = TrainingListItemContract.Columns.DISTANCE)
     val distance: Float,
 
-    @ColumnInfo(name = TrainingContract.Columns.MOVING_TIME)
+    @ColumnInfo(name = TrainingListItemContract.Columns.MOVING_TIME)
     val movingTime: Int,
 
-    @ColumnInfo(name = TrainingContract.Columns.TYPE)
+    @ColumnInfo(name = TrainingListItemContract.Columns.TYPE)
     val type: String,
 
-    @ColumnInfo(name = TrainingContract.Columns.START_DATE)
+    @ColumnInfo(name = TrainingListItemContract.Columns.START_DATE)
     val startDate: Date
 )
