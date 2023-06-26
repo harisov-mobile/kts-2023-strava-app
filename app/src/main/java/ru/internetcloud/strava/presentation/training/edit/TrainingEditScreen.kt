cd6fe4cd17a6bc8a14f5074c73633641d0005479
harisov-mobile <v8spec@mail.ru>
2023-06-26 19:04:17 +0300

    Fix TrainingEditScreen

--- app/src/main/java/ru/internetcloud/strava/presentation/training/edit/TrainingEditScreen.kt
+++ app/src/main/java/ru/internetcloud/strava/presentation/training/edit/TrainingEditScreen.kt
@@ -42,6 +42,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -80,7 +81,7 @@ fun TrainingEditScreen(
 ) {
     val context = LocalContext.current
 
-    val shouldExitHere = remember { mutableStateOf(false) }
+    val shouldExitHere = rememberSaveable { mutableStateOf(false) }
 
     val viewModel: TrainingEditViewModel by viewModel {
         parametersOf(trainingId, editMode)
